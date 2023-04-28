e193de5b3a1a0d4d4943ce4f67f20d085316e489
BoilingSODA <92413560+Go2gym@users.noreply.github.com>
2023-04-28 22:03:07 +0900

    Create Longest Common Prefix.cc
    https://leetcode.com/problems/longest-common-prefix/

---
+++ LeetCode/Longest Common Prefix.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        sort(strs.begin(), strs.end());
+        string n = strs[0], m = strs[strs.size()-1], res = "";
+
+        for (int i = 0; i < n.size(); i++)
+        {
+            if(n[i] == m[i])
+            {
+                res += n[i];
+            }
+            else
+                break;
+        }
+        return res;
+    }
+};
