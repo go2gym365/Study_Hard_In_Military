6b25b2068840b3f2b5e0254d8de03ba3c524e7a1
BoilingSODA <92413560+Go2gym@users.noreply.github.com>
2023-04-28 19:42:22 +0900

    Update 모의고사.cc
    https://www.notion.so/8d260693c9994df0bd8e7315f97eb10b
    https://school.programmers.co.kr/learn/courses/30/lessons/42840

--- Programmers/모의고사.cc
+++ Programmers/모의고사.cc
@@ -1,8 +1,41 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main() 
-{
-    cout << "hello" endl;
+vector<int> solution(vector<int> answers) {
+    vector<pair<int, int>> answer;
+    vector<int> ans;
+    vector<int> p1 { 1, 2, 3, 4, 5 };
+    vector<int> p2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    vector<int> p3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    
+    int P1 = 0, P2 = 0, P3 = 0;
+    
+    for(int i = 0; i < answers.size() ; i++)
+    {
+        P1 += p1[i % p1.size()] == answers[i] ? 1 : 0;
+        P2 += p2[i % p2.size()] == answers[i] ? 1 : 0;
+        P3 += p3[i % p3.size()] == answers[i] ? 1 : 0;
+    }
+    
+    answer.push_back({P1, 1});
+    answer.push_back({P2, 2});
+    answer.push_back({P3, 3});
+
+    sort(answer.begin(), answer.end(), greater<>());
+    
+    int best = answer[0].first;
+    ans.push_back(answer[0].second);
+    
+    for(auto a : answer)
+    {
+        if(best == a.first)
+            ans.push_back(a.second);
+        else
+            break;
+    }
+    
+    sort(ans.begin(), ans.end());
+    
+    return ans;
 }
