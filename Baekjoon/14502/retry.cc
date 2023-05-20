30b31236efd224f74170f81f3194c523c0333b96
BubblingSODA <92413560+Go2gym@users.noreply.github.com>
2023-05-20 14:55:12 +0000

    14502

---
+++ Baekjoon/14502/retry.cc
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<vector<int>>lab(8, vector<int>(8, 0));
+vector<vector<bool>>visited(8, vector<bool>(8, 0));
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int answer = 0;
+
+int BFS()
+{
+    queue<pair<int, int>> q;
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(lab[i][j] == 2)
+                q.push({i, j});
+        }
+    }
+
+    vector<pair<int, int>> virus;
+
+    while(!q.empty())
+    {
+        int x = q.front().second;
+        int y = q.front().first;
+        q.pop();
+
+        for(int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(ny >= N || ny < 0 || nx < 0 || nx >= M)
+                continue;
+            if(lab[ny][nx] == 0)
+            {
+                lab[ny][nx] = 2;
+                virus.push_back({ny, nx});
+                q.push({ny, nx});
+            }            
+        }
+    }
+    
+    int ret = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (lab[i][j] == 0) 
+            {
+                ret++;
+            }
+                
+        }
+    }
+
+    for (auto v : virus)
+    {
+        lab[v.second][v.first] = 0;
+    }
+    
+    return ret;
+}
+
+void combination(int cnt)
+{
+    if(cnt == 3)
+    {
+        int res = BFS();
+        answer = max(res, answer);
+        return;
+    }
+
+    for(int i=0; i<N; i++)
+     {
+        for(int j=0; j<M; j++) 
+        {
+            if(lab[i][j] != 0) 
+                continue;
+            lab[i][j] = 1;
+            combination(cnt + 1);
+            lab[i][j] = 0;
+        }
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            cin >> lab[i][j];
+        }
+    }
+
+    combination(0);
+
+    cout << answer;
+}
