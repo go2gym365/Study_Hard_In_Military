20c4a01d97fb22e49b85b22cc16eb7fdcd3ace05
Boiling SODA <go2gym365@gmail.com>
2023-06-02 00:12:49 +0000

    ds

---
+++ Baekjoon/1018/main.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char arr[51][51];
+char white_arr[8][8] = {
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W'
+};
+
+char black_arr[8][8] = {
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B',
+	'B','W','B','W','B','W','B','W',
+	'W','B','W','B','W','B','W','B'
+};
+
+int white_first(int x, int y) {
+	int result = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (arr[x + i][y + j] != white_arr[i][j])
+				result++;
+		}
+	}
+	return result;
+}
+
+int black_first(int x, int y) {
+	int result = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (arr[x + i][y + j] != black_arr[i][j])
+				result++;
+		}
+	}
+	return result;
+}
+
+int main() {
+    int n, m, result = 64;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> arr[i][j];
+    
+    int t_white, t_black;
+}
