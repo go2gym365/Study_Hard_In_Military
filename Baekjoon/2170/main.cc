6d0284d2c656e903e2cfdb8c31e5452b03d32b7a
BubblingSODA <go2gym365@gmail.com>
2023-06-05 10:17:58 +0000

    2170

--- Baekjoon/2170/main.cc
+++ Baekjoon/2170/main.cc
@@ -4,22 +4,20 @@ using namespace std;
 
 vector<pair<int, int>> v;
 
-bool Compare(pair<int, int> A, pair<int, int> B)
-{
+bool Compare(pair<int, int> A, pair<int, int> B) {
     if (A.first == B.first)
         return A.second < B.second;
     return A.first < B.first;
 }
 
-int main()
-{
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n;
-    int cnt = 0;
-    int temp = 0;
+    int cnt;
+    int temp;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         int start, end;
         cin >> start >> end;
         v.push_back({start, end});
@@ -27,18 +25,18 @@ int main()
 
     sort(v.begin(), v.end(), Compare);
 
-    for (int i = 0; i < n; i++)
-    {
-        if (temp < v[i].second && temp >= v[i].first)
-        {
+    cnt = v[0].second - v[0].first;
+    temp = v[0].second;
+
+    for (int i = 1; i < n; i++) {
+        if (temp < v[i].second && temp >= v[i].first) {
             cnt = cnt + (v[i].second - temp);
         }
-        else if (temp < v[i].first)
-        {
+        else if (temp < v[i].first) {
             cnt += v[i].second - v[i].first;
         }
+        else if(v[i].first <= temp && v[i].second <= temp) continue;
         temp = v[i].second;
     }
-
     cout << cnt;
 }
