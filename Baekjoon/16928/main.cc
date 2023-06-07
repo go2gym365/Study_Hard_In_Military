7bfc88cabdfce6e758457ca97327587c61ea4bd1
BubblingSODA <go2gym365@gmail.com>
2023-06-07 11:57:41 +0000

    16928

---
+++ Baekjoon/16928/main.cc
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> v(102, 0);
+vector<bool> vis(102, false);
+
+void BFS(int x, int cnt) {
+    queue<pair<int, int>> q;
+    q.push({x, cnt});
+    
+    while(!q.empty()) {
+        int location = q.front().first;
+        int count = q.front().second;
+        q.pop();
+
+        for(int i = 1; i <= 6; i++) {
+            int nx = location + i;
+            if(nx == 100) {
+                cout << count + 1;
+                return;
+            }
+            else if(nx < 100) {
+                while(v[nx] != 0) {
+                    nx = v[nx];
+                }
+                if(!vis[nx]){
+                    q.push({nx, count + 1});
+                    vis[nx] = true;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    
+    for(int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        v[a] = b;
+    }
+
+    for(int i = 0; i < M; i++) {
+        int c, d;
+        cin >> c >> d;
+
+        v[c] = d;
+    }
+
+    BFS(1, 0);
+}
+https://hagisilecoding.tistory.com/85
