edfbaf8dbaa01e0938d036268757fa10de23fe11
BubblingSODA <go2gym365@gmail.com>
2023-06-11 12:22:49 +0000

    2138

--- Baekjoon/2138/main.cc
+++ Baekjoon/2138/main.cc
@@ -2,28 +2,43 @@
 
 using namespace std;
 
-vector<int> v(10001, 0);
+string ex, user, temp;
+int cnt = 0;
+int ans = 987654321;
+int n;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+void lightOn(int idx) {
+    if(idx > 0) temp[idx - 1] = (temp[idx - 1] == '0') ? '1' : '0';
+    temp[idx] = (temp[idx] == '0') ? '1' : '0';
+    if(idx < n - 1) temp[idx + 1] = (temp[idx + 1] == '0') ? '1' : '0';
+}
 
-    int n;
-    cin >> n;
-
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-
-        v[a]++;
+void solve(int start) {
+    temp = user;
+    cnt = 0;
+    if(start == 0) {
+        temp[0] = (temp[0] == '0') ? '1' : '0';
+        temp[1] = (temp[1] == '0') ? '1' : '0';
+        ++cnt;
     }
-
-    sort(v.begin(), v.end());
-
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < v[i]; j++) {
-            cout << i;
+        if(temp[i - 1] != ex[i-1]) {
+            lightOn(i);
+            ++cnt;
         }
     }
+    if(temp == ex) 
+        ans = min(ans, cnt);
+}
+
+int main() {
+    cin >> n;
+    cin >> user;
+    cin >> ex;
+
+    solve(1);
+    solve(0);
+
+    if(ans != 987654321) cout << ans;
+    else cout << -1;
 }
