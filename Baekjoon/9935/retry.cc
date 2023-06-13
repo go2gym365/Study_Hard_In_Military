3a66b44ea1f06e5c7cb2a11a571518ef7d9534da
BubblingSODA <go2gym365@gmail.com>
2023-06-13 10:40:15 +0000

    9935

--- Baekjoon/9935/retry.cc
+++ Baekjoon/9935/retry.cc
@@ -4,12 +4,28 @@ using namespace std;
 
 int main() {
     string str, boom;
-    string temp;
+    string temp = "";
 
     cin >> str;
     cin >> boom;
 
     for(int i = 0; i < str.size(); i++) {
-        
+        temp += str[i];
+        if(temp.back() == boom.back()) {
+            bool check = true;
+            for(int j = 0; j < boom.size(); j++) {
+                if(temp[temp.size() - boom.size() + j] != boom[j]) {
+                    check = false;
+                    continue;
+                }
+            }
+            if(check) {
+                for(int j = 0; j < boom.size(); j++) {
+                    temp.pop_back();
+                }
+            }
+        }
     }
+    if(temp.empty()) cout << "FRULA";
+    else cout << temp;
 }
