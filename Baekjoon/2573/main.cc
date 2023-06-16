82539b3f905d391c0e3758bf1866c04d8af8b09f
BubblingSODA <go2gym365@gmail.com>
2023-06-16 09:57:29 +0000

    2573

--- Baekjoon/2573/main.cc
+++ Baekjoon/2573/main.cc
@@ -6,49 +6,45 @@ vector<vector<int>> v(301, vector<int>(301, 0));
 vector<vector<int>> cntv(301, vector<int>(301, 0));
 vector<vector<bool>> vis(301, vector<bool>(301, false));
 
-int n, m;
+int n, m, result = 0;
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-bool isIce() {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(v[i][j] > 0) return true;
-        }
-    }
-    return false;
-}
-
-void melt_cnt(int y, int x) {
-    for(int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-        if(v[ny][nx] <= 0) cntv[y][x]++;
-    }
-}
-
-void melt() {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            v[i][j] = v[i][j] - cntv[i][j];
-        }
-    }
-}
-
-void DFS(int yy, int xx) {
-    int cnt_0 = 0;
+void DFS(int yy, int xx, int cnt) {
+    cntv[yy][xx] = cnt;
 
     for(int i = 0; i < 4; i++) {
         int nx = xx + dx[i];
         int ny = yy + dy[i];
 
-        if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-        if(v[ny][nx] <= 0 || vis[ny][nx]) continue;
-        vis[ny][nx] == true;
-        DFS(ny, nx);
+        if (ny >= 0 && ny < n && nx >= 0 && nx < m)
+			if (v[ny][nx] && cntv[ny][nx] == 0) 
+				DFS(ny, nx, cnt);
     }
+}
+
+void melt_ice() {
+    int melt = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(v[i][j] == 0) continue;
+            melt = 0;
+            for(int k = 0; k < 4; k++) {
+                int nx = j + dx[k];
+                int ny = i + dy[k];
+                if (v[ny][nx] == 0 && ny >= 0 && ny < n && nx >= 0 && nx < m)
+                    melt++;
+            }
+
+            if(v[i][j] < melt) continue;
+            else cntv[i][j] == v[i][j] - melt;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++)
+            v[i][j] = cntv[i][j];
+    }    
 }
 
 int main() {
@@ -57,48 +53,44 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> v[i][j];
-            if(v[i][j] != 0) vis[i][j] = true;
         }
     }
 
-    int year = 0;
-    
     while(1) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                vis[i][j] = 0;
-                cntv[i][j] = 0;
-            }
-        }
-
-        if(isIce() == false) {
-            cout << "0";
-            break;
-        }
-
+        //빙산이 몇조각인지 확인하기
+        fill(cntv.begin(), cntv.end(), vector<int> (301, 0));
         int cnt = 0;
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(v[i][j] > 0 && !vis[i][j]) {
+            for(int j = 0; i < m; j++) {
+                if(v[i][j] && cntv[i][j] == 0) {
                     cnt++;
-                    vis[i][j] = 1;
-                    DFS(i, j);
+                    DFS(i, j, cnt);
                 }
             }
         }
-
-        if(cnt > 1) {
-            cout << year;
-            break;
+        // 2. 두조각 이상인지 확인하기
+        if(cnt >= 2) {
+            cout << result << "\n";
+            return 0;
         }
-        year++;
 
+        //3. 얼음 녹이기
+        fill(cntv.begin(), cntv.end(), vector<int>(301, 0));
+        melt_ice();
+
+        // 3. 얼음이 다 녹은지 확인
+        bool jud = false;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(v[i][j] > 0) melt_cnt(i, j);
+                if(v[i][j]) jud = true;
             }
         }
+        if(jud == false) {
+            cout << 0 << "\n";
+            return 0;
+        }
 
-        melt();
+        //카운트 추가
+        result++;
     }
 }
