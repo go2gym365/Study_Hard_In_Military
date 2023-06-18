2b8cf0083a6af9c2f6a0c3ff2fec9ef207173c88
BubblingSODA <go2gym365@gmail.com>
2023-06-18 23:31:15 +0000

    9095

---
+++ Baekjoon/9095/recur.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int dp[15];
+int N;
+
+int solve(int cur) {
+    if(cur == 0) return 1;
+    if(cur < 0) return 0;
+    int &ret = dp[cur];
+    if(ret != -1) return ret;
+    ret = 0;
+    ret += solve(cur - 1);
+    ret += solve(cur - 2);
+    ret += solve(cur - 3);
+    return ret;
+}
+
+int main() {
+    memset(dp, -1, sizeof(dp));
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T;
+    cin >> T;
+    while(T--) {
+        cin >> N;
+        cout << solve(N) << '\n';
+    }
+
+    for(int i=0; i<=10; i++) {
+        cout << dp[i] << " ";
+    }
+}
