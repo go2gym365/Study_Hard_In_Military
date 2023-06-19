20fe355e78633794994a7468a87bbe12e562e615
BubblingSODA <go2gym365@gmail.com>
2023-06-19 11:53:29 +0000

    1106

---
+++ Baekjoon/1106/main.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> ads;
+int dp[21][101];
+
+int c, n;
+
+// ret : 돈의 최솟값,
+
+int solve(int visiter, int num)
+{
+    if (visiter >= c) return 0;
+    if(num >= n) return 1000000;
+    int &ret = dp[num][visiter];
+    if (ret != -1) return ret;
+    ret = 987654321;
+
+    int mul = 0;
+    while(visiter + ads[num].second * mul < c) {
+        ret = min(ret, solve(ads[num].second * mul + visiter, num+1) + ads[num].first * mul);
+        ++mul;
+    }
+    ret = min(ret, solve(ads[num].second * mul + visiter, num+1) + ads[num].first * mul);
+
+    return ret;
+}
+
+int main()
+{
+    memset(dp, -1, sizeof(dp));
+    cin >> c >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int cost, cust;
+        cin >> cost >> cust;
+
+        ads.push_back({cost, cust});
+    }
+
+    cout << solve(0, 0);
+}
