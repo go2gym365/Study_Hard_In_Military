b99eab9f7b2559798f1367bc41a2488ab0719a73
Boiling SODA <go2gym365@gmail.com>
2023-06-20 19:08:02 +0000

    11651

---
+++ Baekjoon/11651/main.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> v;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    if(a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+    sort(v.begin(), v.end(), cmp);
+    
+    for(int i = 0; i < n; i++) {
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+}
