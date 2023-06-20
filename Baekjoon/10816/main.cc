1971fd91620caac9943e7c58446036ece2dbf984
BubblingSODA <go2gym365@gmail.com>
2023-06-20 10:48:22 +0000

    .

---
+++ Baekjoon/10816/main.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[500001];
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        
+    }
+}
