445319c68b7498349880624fc86d8c57c6f21687
BubblingSODA <go2gym365@gmail.com>
2023-06-20 09:32:42 +0000

    1920

--- Baekjoon/1106/main.cc
+++ Baekjoon/1106/main.cc
@@ -29,6 +29,7 @@ int solve(int visiter, int num)
 
 int main()
 {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     memset(dp, -1, sizeof(dp));
     cin >> c >> n;
 
---
+++ Baekjoon/1920/main.cc
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int num[100001];
+
+void binarySearch(int target, int arr[]) {
+    int low = 0;
+    int high = n;
+    int mid;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(num[mid] == target) {
+            cout << '1' << "\n";
+            return;
+        }
+        else if(num[mid] < target) {
+            low = mid + 1;
+        }
+        else if(num[mid] > target) {
+            high = mid - 1;
+        }
+    }
+    cout << '0' << "\n";
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        num[i] = a;
+    }
+
+    sort(num, num+n);
+
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        int b;
+        cin >> b;
+        binarySearch(b, num);
+    }
+}
