440c54812f3ca74420d38b6a013a9cf457126b20
Boiling SODA <go2gym365@gmail.com>
2023-06-22 16:49:48 +0000

    18111

--- Baekjoon/18111/main.cc
+++ Baekjoon/18111/main.cc
@@ -5,76 +5,12 @@ using namespace std;
 int n, m, b;
 
 vector<vector<int>> v(501, vector<int>(501, 0));
-vector<pair<int, int>> ans;
 
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    if(a.first == b.first) return a.second > b.second;
-    return a.first < b.first;
-}
-
-void dig() {
-    int sec = 0;
-    int first = 987654321;
-
-    for(int i= 0; i < n; i++) {
-        for(int j = 0; j < m; j++){
-            first = min(first, v[i][j]);
-        }
-    }
-
-    for(int i= 0; i < n; i++) {
-        for(int j = 0; j < m; j++){
-            if(v[i][j] != first) {
-                sec += (v[i][j] - first) * 2;
-                v[i][j] = first;
-            }
-        }
-    }
-    ans.push_back({sec, v[0][0]});
-}
-
-void struc() {
-    int sec = 0;
-    int mx = -987654321;
-
-    for(int i= 0; i < n; i++) {
-        for(int j = 0; j < m; j++){
-            mx = max(mx, v[i][j]);
-        }
-    }
-
-    for(int i= 0; i < n; i++) {
-        for(int j = 0; j < m; j++){
-            if(v[i][j] != mx) {
-                if(b <= mx - v[i][j]) {
-                    for(int i = 0; i < n; i++) {
-                        for(int j = 0; j < m; j++) {
-                            mx = max(mx, v[i][j]);
-                        }
-                    }
-                    ans.push_back({sec, mx});    
-                    return;
-                }
-                sec += mx - v[i][j];
-                b -= mx - v[i][j];
-                v[j][i] = mx;
-            }
-            else continue;
-        }
-    }
-
-    while(b > n * m) {
-        v[0][0]++;
-        sec += n * m;
-        b -= n * m;
-    }
-
-    ans.push_back({sec, v[0][0]});
-}
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0); cin.tie(0);
-
+    int leastTime = 987654321;
+    int mostHeight;
     cin >> n >> m >> b;
 
     for(int i= 0; i < n; i++) {
@@ -83,8 +19,24 @@ int main() {
         }
     }
 
-    dig();
-    struc();
-    sort(ans.begin(), ans.end(), cmp);
-    cout << ans.front().first << " " << ans.front().second;
+    for(int i = 0; i <= 256; i++) {
+        int build = 0;
+        int remove = 0;
+
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < m; k++) {
+                int height = v[j][k] - i;
+                if(height > 0) remove += height;
+                else if(height < 0) build -= height;
+            }
+        }
+        if(remove + b >= build) {
+            int time = remove * 2 + build;
+            if(leastTime >= time) {
+                leastTime = time;
+                mostHeight = i;
+            }
+        }
+    }
+    cout << leastTime << ' ' << mostHeight;
 }
