6370408bdd176554e66c66751f9e94dd3d1ad990
Boiling SODA <go2gym365@gmail.com>
2023-06-25 17:46:27 +0000

    2775

---
+++ Baekjoon/2775/main.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int dp[15];
+
+int main() {
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        int k, n;
+        cin >> k >> n;
+
+        
+    }
+}
