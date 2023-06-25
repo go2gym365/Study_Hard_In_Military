04437ea54d93eee29d8b7f7f184e7f55e4463f95
Boiling SODA <go2gym365@gmail.com>
2023-06-25 04:15:22 +0000

    a

---
+++ Competition/2nd_chocolatecup/A_main.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool solve(string str) {
+    if()
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        string str;
+        cin >> str;
+
+        solve(str);
+    }
+}
