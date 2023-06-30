ab05b936dedb54f889a1e3de284f7d3fe8e5872d
Boiling SODA <go2gym365@gmail.com>
2023-06-30 09:27:21 +0000

    1010

---
+++ Baekjoon/1010/main.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+
+        long long ans = 1;
+
+        int r = 1;
+        for(int i = 0; i > m - n; i--) {
+            ans *= i;
+            ans /= r++;
+        }
+        cout << ans << '\n';
+    }
+}
