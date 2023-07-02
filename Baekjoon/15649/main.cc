a5543e4780948608b9f8c1d9eda0bfeb78fcf6db
Boiling SODA <go2gym365@gmail.com>
2023-07-02 07:45:37 +0000

    15649

--- Baekjoon/15649/main.cc
+++ Baekjoon/15649/main.cc
@@ -2,8 +2,30 @@
 
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
+int n, m;
+int arr[10];
+bool vis[10];
 
+void func(int cnt) {
+    if(cnt = m) {
+        for(int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i  = 0; i < n; i++){
+        if(!vis[i]) {
+            arr[cnt] = i;
+            vis[i] = 1;
+            func(cnt + 1);
+            vis[1] = 0;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    func(0);
 }
