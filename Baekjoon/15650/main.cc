8120b0c3976aa43e675bfbdac13ab45515336c97
Boiling SODA <go2gym365@gmail.com>
2023-07-02 13:39:29 +0000

    15650

---
+++ Baekjoon/15650/main.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+vector<int> (8, 0);
+vector<bool> (8, false);
+
+void DFS(int cnt) {
+    if(cnt == )
+}
+
+int main() {
+    cin >> n >> m;
+    DFS(0);
+}
