fcf4a6ac69f9bb0895e77f926b8db90f761bf8cf
Boiling SODA <go2gym365@gmail.com>
2023-07-02 17:06:26 +0000

    15655

---
+++ Baekjoon/15655/mian.cc
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+vector<int> v(10, 0);
+vector<bool> vis(10, false);
+vector<int> k;
+
+void DFS(int cnt, int cur) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i = cur; i < n; i++) {
+        if(!vis[i]) {
+            vis[i] = true;
+            v[cnt] = k[i];
+            DFS(cnt + 1, i + 1);
+            vis[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        k.push_back(a);
+    }
+
+    sort(k.begin(), k.end());
+
+    DFS(0, 0);
+}
