f546ad518de6f19c47fd5374caa8de05c34ed433
BubblingSODA <go2gym365@gmail.com>
2023-07-03 11:58:11 +0000

    14940

--- Baekjoon/14940/main.cc
+++ Baekjoon/14940/main.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 vector<vector<int>> v(1001, vector<int>(1001, 0));
-vector<vector<int>> vis(1001, vector<int>(1001, 0));
+vector<vector<bool>> vis(1001, vector<bool>(1001, false));
 
 int n, m;
 int dx[4] = {-1, 0, 1, 0};
@@ -13,7 +13,8 @@ void BFS(int yy, int xx) {
     queue<pair<int, int>> q;
     q.push({yy, xx});
 
-
+    v[yy][xx] = 0;
+    vis[yy][xx] = true;
 
     while (!q.empty()) {
         int x = q.front().second;
@@ -25,11 +26,14 @@ void BFS(int yy, int xx) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if(v[ny][nx] == 0) continue;
-            if(vis[ny][nx] != 0) continue;
-            if(ny == 0 && nx == 0) continue;
+            if(vis[ny][nx]) continue;
+            if(v[ny][nx] == 0) {
+                vis[ny][nx] = true;
+                continue;
+            }
+            v[ny][nx] = v[y][x] + 1;
+            vis[ny][nx] = true;
             q.push({ny, nx});
-            vis[ny][nx] += vis[y][x] + 1;
         }
     }
 }
@@ -51,8 +55,13 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cout << vis[i][j] << " ";
+            if(!vis[i][j]) {
+                cout << "-1" << " ";
+            }
+            else cout << v[i][j] << " ";
         }
         cout << "\n";
     }
+
+    
 }
