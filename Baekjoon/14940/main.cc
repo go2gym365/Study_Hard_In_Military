15ba08be61e8280d741a49839727d7f5e2633cb7
Boiling SODA <go2gym365@gmail.com>
2023-07-03 10:35:30 +0000

    14940

--- Baekjoon/14940/main.cc
+++ Baekjoon/14940/main.cc
@@ -1,9 +1,9 @@
-#include<bits/stdc++/.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 vector<vector<int>> v(1001, vector<int>(1001, 0));
-vector<vector<int>> vis(1001, vector<int>(1001, -1));
+vector<vector<int>> vis(1001, vector<int>(1001, 0));
 
 int n, m;
 int dx[4] = {-1, 0, 1, 0};
@@ -13,9 +13,9 @@ void BFS(int yy, int xx) {
     queue<pair<int, int>> q;
     q.push({yy, xx});
 
-    vis[yy][xx] = 0;
 
-    if(!q.empty()) {
+
+    while (!q.empty()) {
         int x = q.front().second;
         int y = q.front().first;
         
@@ -26,29 +26,31 @@ void BFS(int yy, int xx) {
             int ny = y + dy[i];
             if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
             if(v[ny][nx] = 0) continue;
+            if(vis[ny][nx] != 0) continue;
+            q.push({ny, nx});
+            vis[ny][nx] += vis[y][x] + 1;
         }
     }
 }
 
 int main() {
     cin >> n >> m;
-
+    int a, b;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin > v[i][j];
+            cin >> v[i][j];
+            if(v[i][j] == 2) {
+                a = i;
+                b = j;
+            }
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(v[i][j] == 2) 
-                BFS(i, j);
-        }
-    }
+    BFS(a, b);
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cout << v[i][j] << " ";
+            cout << vis[i][j] << " ";
         }
         cout << "\n";
     }
