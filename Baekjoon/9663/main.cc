f28f301c9f3936b166a096b4fa15d050c99b7fd5
BubblingSODA <go2gym365@gmail.com>
2023-07-04 11:04:09 +0000

    9663

---
+++ Baekjoon/9663/main.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> vec(16, vector<int>(16, 0));
+vector<vector<bool>> vec(16, vector<bool>(16, 0));
+
+int n;
+
+int main() {
+    cin >> n;
+
+    solve();
+}
