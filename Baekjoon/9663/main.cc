e3504ebc6b0ad690900a02322994cecff181c407
BubblingSODA <go2gym365@gmail.com>
2023-07-05 10:24:09 +0000

    9663

--- Baekjoon/9663/main.cc
+++ Baekjoon/9663/main.cc
@@ -7,7 +7,14 @@ vector<vector<bool>> vec(16, vector<bool>(16, 0));
 
 int n;
 
-
+void queen(int x) {
+    if(n == x) ans++;
+    else {
+        for(int i = 0; i < n; i++) {
+            
+        }
+    }
+}
 
 int main() {
     cin >> n;
