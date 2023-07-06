179e9033165f3145af1473ee8c502c2db0768eb5
BubblingSODA <go2gym365@gmail.com>
2023-07-06 12:18:47 +0000

    1107

--- Baekjoon/1107/main.cc
+++ Baekjoon/1107/main.cc
@@ -2,76 +2,36 @@
 
 using namespace std;
 
-vector<int> v;
-
 int n, m;
-int ans = 987654321;
-int value;
 
-bool numCheck(int num) {
-    string str = to_string(num);
-    
-    for(int i = 0; i < str.size(); i++) {
-        for(int j = 0; j < v.size(); j++) {
-            if(str[i] != v[j]) return false;
+vector<bool> num(10, false);
+
+bool check(int now) {
+    string s = to_string(now);
+    for(int i = 0; i < s.length(); i++) {
+        if(num[s[i] - 48]) {
+            return false;
         }
     }
     return true;
 }
 
-void selNum(int culNum, int cnt) {
-    if(culNum > 1000001) return;
-    if(culNum == m) {
-        if (ans > cnt) {
-            ans = cnt;
-            value = culNum;
-        }
-        return;
-    }
-    else {
-        int temp = cnt + abs(m - culNum);
-        if(ans > temp) {
-            ans = temp;
-            value = culNum;
-        }
-    }
-
-    for(int i  = 0; i < 10; i++) {
-        if(culNum == 0 && i == 0) continue; 
-        if(find(v.begin(), v.end(), i) != v.end()) continue;
-        selNum(culNum * 10 + i, cnt + 1);
-    }
-}
-
 int main() {
     cin >> n >> m;
 
-    if(m != 0) {
-        for(int i = 0; i < m; i++){
-            int a;
-            cin >> a;
-            v.push_back(a);
-        }
+    for(int i = 0; i < m; i++) {
+        int a;
+        cin >> a;
+        num[a] = true;
     }
 
-    if(m == 0 || numCheck(n)) {
-        int cnt = 0;
-        int temp = n;
-        while(temp > 0) {
-            temp /= 10;
-            cnt++;
-        }
-        if(ans > cnt) {
-            ans = cnt;
-            value = n; 
+    int ans = abs(n - 100);
+    
+    for(int i = 0; i <= 1000000; i++) {
+        if(check(i)) {
+            int res =  abs(n - i) + to_string(i).size();
+            ans = min(ans, res);
         }
     }
-
-    if(!numCheck(n)) {
-        selNum(0, 0);
-    }
-
-    ans = min(ans, abs(m - 100));
-
-    cout << ans << " " << value;
+    cout << ans;
 }
