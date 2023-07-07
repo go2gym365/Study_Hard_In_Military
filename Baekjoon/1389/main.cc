d7adf6867da76772e2cd2bfdb750d849a2a154dc
BubblingSODA <go2gym365@gmail.com>
2023-07-07 13:31:22 +0000

    1389

--- Baekjoon/1389/main.cc
+++ Baekjoon/1389/main.cc
@@ -3,11 +3,24 @@
 using namespace std;
 
 vector<int> vec[101];
+vector<bool> vis(101, false);
 
 int n, m;
+int cnt = 0;
 
 void BFS(int start) {
+    queue<int> q;
+    q.push(1);
 
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for(int i = 0; i < vec[cur].size(); i++) {
+            int next = vec[cur][i];
+            
+        }
+    }
 }
 
 int main() {
