e09cce199f07db7c6618782889eac811c7d27a27
BubblingSODA <go2gym365@gmail.com>
2023-07-09 06:59:02 +0000

    17416

--- Baekjoon/17413/main.cc
+++ Baekjoon/17413/main.cc
@@ -10,34 +10,25 @@ int main() {
 
     for(int i = 0; i <= str.size(); i++) {
         if(str[i] == '<') {
-            if(!sta.empty()) {
-                while(!sta.empty()) {
-                cout << sta.top();
-                sta.pop();
-                }
-            }
-            while(1) {
-                cout << str[i];
-                i += 1;
-                if(str[i] == '>') {
-                    cout << str[i];
-                    i += 1;
-                    break;
-                }
-            }
-        }
-
-
-        if(str[i] == ' ') {
             while(!sta.empty()) {
                 cout << sta.top();
                 sta.pop();
             }
+            while(1) {
+                cout << str[i];
+                if(str[i] == '>') 
+                    break;
+                i += 1;
+            }
         }
-        
-        if(str[i] == ' ')
-            cout << ' ';
-        else
+        else if(str[i] == ' ') {
+            while(!sta.empty()) {
+                cout << sta.top();
+                sta.pop();
+            }
+            cout << " ";
+        }
+        else 
             sta.push(str[i]);
     }
     while(!sta.empty()) {
