5bbccb387b1b83e16fdec40fe4f77fd1b3bf2029
BubblingSODA <go2gym365@gmail.com>
2023-07-09 05:59:43 +0000

    17413

--- Baekjoon/17413/main.cc
+++ Baekjoon/17413/main.cc
@@ -2,11 +2,38 @@
 
 using namespace std;
 
+stack<char> sta;
+
 int main() {
     string str;
     getline(cin, str);
 
-    for(int i = 0; i < str.size(); i++) {
-        if()
+    for(int i = 0; i <= str.size(); i++) {
+        if(str[i] == '<') {
+            if(!sta.empty()) {
+                while(!sta.empty()) {
+                cout << sta.top();
+                sta.pop();
+                }
+            }
+            while(1) {
+                cout << str[i];
+                i += 1;
+                if(str[i] == '>') break;
+            }
+        }
+
+        sta.push(str[i]);
+
+        if(str[i] == ' ') {
+            while(!sta.empty()) {
+                cout << sta.top();
+                sta.pop();
+            }
+        }
+    }
+    while(!sta.empty()) {
+        cout << sta.top();
+        sta.pop();
     }
 }
