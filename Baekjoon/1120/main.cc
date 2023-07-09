e844d214e1058ae4946762d5d4e517a04493a09e
BubblingSODA <go2gym365@gmail.com>
2023-07-09 16:47:34 +0000

    1120

--- Baekjoon/1120/main.cc
+++ Baekjoon/1120/main.cc
@@ -4,12 +4,19 @@ using namespace std;
 
 int main() {
     string str1, str2;
-
+    int count = 0;
+    int result = 0x3f3f3f3f;
+    
     cin >> str1 >> str2;
 
-    while(str1.size() != str2.size()) {
-        for(int i = 0; i < 26; i++) {
+    for(int i = 0; i <= str2.size() - str1.size(); i++) {
+        count = 0;
 
+        for(int j = 0; j < str1.length(); j++) {
+            if (str1[j] != str2[j + i]) 
+                count++;
         }
+        result = min(result, count);
     }
+    cout << result;
 }
