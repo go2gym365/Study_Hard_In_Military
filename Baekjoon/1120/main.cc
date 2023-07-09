6dee27257b5f5d890c7ec5961e38317243ce106b
BubblingSODA <go2gym365@gmail.com>
2023-07-09 13:45:08 +0000

    1120

---
+++ Baekjoon/1120/main.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    while(str1.size() != str2.size()) {
+        for(int i = 0; i < 26; i++) {
+
+        }
+    }
+}
