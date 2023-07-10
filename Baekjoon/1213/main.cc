a887dddbd5c6c4517e8b576f1082fc0087ee8c06
BubblingSODA <go2gym365@gmail.com>
2023-07-10 10:24:43 +0000

    1213

--- Baekjoon/1213/main.cc
+++ Baekjoon/1213/main.cc
@@ -4,11 +4,13 @@ using namespace std;
 
 int alpha[26];
 
+
+//알파벳의 갯수가 홀수인 알파벳이 2개 이상이면 펠린드롬 못만듬
 bool cannotPalin() {
     int cnt = 0;
     for(int i = 0; i < 26; i++) {
         if(alpha[i] % 2 == 1)
-            cnt++
+            cnt++;
     }
     return cnt > 1;
 }
@@ -31,4 +33,17 @@ int main() {
             ans += i + 'A';
         }
     }
+
+    for(int i = 0; i < 26; i++) {
+        if(alpha[i] % 2) {
+            ans += i + 'A';
+        }
+    }
+    for(int i = 25; i >= 0; i--) {
+        for(int j = 0; j < alpha[i] / 2; j++) {
+            ans += i + 'A';
+        }
+    }
+
+    cout << ans;
 }
