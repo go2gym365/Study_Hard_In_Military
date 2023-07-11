7ef7b6d70403bb74c429fe08ab29eb74afcf4622
BubblingSODA <go2gym365@gmail.com>
2023-07-11 13:40:02 +0000

    10026

--- Baekjoon/10026/main.cc
+++ Baekjoon/10026/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 int n;
 
 vector<string> vec(101, "");
-vector<vector<bool>> vis(101, vector<bool>(101, 0));
+vector<vector<bool>> vis(101, vector<bool>(101, false));
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
@@ -15,7 +15,7 @@ void BFS(int yy, int xx) {
     q.push({yy, xx});
     vis[yy][xx] = true;
 
-    while(q.empty()) {
+    while(!q.empty()) {
         int x = q.front().second;
         int y = q.front().first;
         q.pop();
@@ -27,6 +27,7 @@ void BFS(int yy, int xx) {
             if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
             if(vis[ny][nx]) continue;
             if(vec[ny][nx] != vec[yy][xx]) continue;
+
             vis[ny][nx] = true;
             q.push({ny, nx});
         }
@@ -44,10 +45,9 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(!vis[i][j]) {
-                BFS(i, j);
-                cntRGB++;
-            }
+            if(vis[i][j]) continue;
+            BFS(i, j);
+            cntRGB++;
         }
     }
 
