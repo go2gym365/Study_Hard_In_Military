d23554bd4114a438cd5413265b77a55388b5a36d
BubblingSODA <go2gym365@gmail.com>
2023-07-11 13:32:14 +0000

    10026

--- Baekjoon/10026/main.cc
+++ Baekjoon/10026/main.cc
@@ -10,7 +10,7 @@ vector<vector<bool>> vis(101, vector<bool>(101, 0));
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void BFS_RG(int yy, int xx) {
+void BFS(int yy, int xx) {
     queue<pair<int, int>> q;
     q.push({yy, xx});
     vis[yy][xx] = true;
@@ -26,31 +26,8 @@ void BFS_RG(int yy, int xx) {
 
             if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
             if(vis[ny][nx]) continue;
-            if(vec[ny][nx] == 'B') continue;
-
-            q.push({ny, nx});
-        }
-    }
-}
-
-void BFS_RGB(int yy, int xx) {
-    queue<pair<int, int>> q;
-    q.push({yy, xx});
-    vis[yy][xx] = true;
-
-    while(q.empty()) {
-        int x = q.front().second;
-        int y = q.front().first;
-        q.pop();
-
-        for(int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if(vis[ny][nx]) continue;
-            if(vec[ny][nx] == 'B') continue;
-
+            if(vec[ny][nx] != vec[yy][xx]) continue;
+            vis[ny][nx] = true;
             q.push({ny, nx});
         }
     }
@@ -67,19 +44,29 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(!vis[i][j] && vec[i][j] == 'R' || vec[i][j] == 'G') {
-                BFS_RG(i, j);
+            if(!vis[i][j]) {
+                BFS(i, j);
+                cntRGB++;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            vis[i][j] = false;
+            if(vec[i][j] == 'R')
+                vec[i][j] = 'G';
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(!vis[i][j]) {
+                BFS(i, j);
                 cntRG++;
             }
         }
     }
-    cout << cntRG << " ";
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(!vis[i][j] && vec[i][j] == 'R' || vec[i][j] == 'G') {
-                BFS_RGB(i, j);
-            }
-        }
-    }
+
+    cout << cntRGB << " " << cntRG;
 }
