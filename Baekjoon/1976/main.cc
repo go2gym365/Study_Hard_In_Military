6b8342fc69842e1ae1bd99527c59db3bde8d5a9a
BubblingSODA <go2gym365@gmail.com>
2023-07-14 11:15:42 +0000

    1976

---
+++ Baekjoon/1976/main.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int vertex[1001];
+
+int main() {
+    for(int i = 1; i <= m; i++) {
+        vertex[i] = i;
+    }
+
+    for(int i = 0; i < m; i++) {
+        int o, a, b;
+        cin >> o >> a >> b;
+
+        if(!o)
+            
+    }
+}
