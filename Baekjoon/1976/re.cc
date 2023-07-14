af74f8d23a6a75f338397225385d99ec44a7d9df
BubblingSODA <go2gym365@gmail.com>
2023-07-14 18:40:24 +0000

    1976

---
+++ Baekjoon/1976/re.cc
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int vertex[201];
+vector<int> path;
+
+int find(int x) {
+    if(vertex[x] == x)
+        return x;
+    return vertex[x] = find(vertex[x]);
+}
+
+void merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x < y) vertex[y] = x;
+    else vertex[x] = y;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    bool check = true;
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++)
+        vertex[i] = i;
+
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            int a;
+            cin >> a;
+            if(a)
+                merge(i, j);
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        int a;
+        cin >> a;
+        path.push_back(a);
+    }
+
+    cin >> path[0];
+    for(int i = 1; i < m; i++) {
+        cin >> path[i];
+        if(find(path[i - 1] != find(path[i]))) {
+            check = false;
+            break;
+        }
+    }
+
+    if(check) cout << "YES";
+    else cout << "NO";
+}
