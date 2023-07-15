f73bea60e3c67533633d40d2e74359e0e2ffeae5
BubblingSODA <go2gym365@gmail.com>
2023-07-15 11:17:17 +0000

    1389

--- Baekjoon/1389/main.cc
+++ Baekjoon/1389/main.cc
@@ -2,14 +2,46 @@
 
 using namespace std;
 
-vector<int> vec[101];
-vector<bool> vis(101, false);
 
 const int INF = 987654321;
 int n, m;
-int cnt = 0;
+int minValue = INF;
+int ans = 0;
 
-void floyd()
+vector<vector<int>> vec(101, vector<int>(101, INF));
+
+void floyd() {
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    vec[i][j] = 0;
+                    continue;
+                }
+                vec[i][j] = min(vec[i][j], vec[i][k] + vec[k][j]);
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (vec[i][j] == INF) {
+                vec[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = 0; j < n; j++) {
+            sum += vec[i][j];
+        }
+        if(minValue > sum) {
+            minValue = sum;
+            ans = i + 1;
+        }
+    }
+}
 
 int main() {
     cin >> n >> m;
@@ -18,9 +50,11 @@ int main() {
         int vertex, edge;
         cin >> vertex >> edge;
 
-        vec[vertex].push_back(edge);
-        vec[edge].push_back(vertex);
+        vec[vertex - 1][edge - 1] = 1;
+        vec[edge - 1][vertex - 1] = 1;
     }
 
-    BFS(1);
+    floyd();
+
+    cout << ans;
 }
