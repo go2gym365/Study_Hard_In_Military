3fda2e42bb6382b86bd506358381e298b6a8c66e
BubblingSODA <go2gym365@gmail.com>
2023-07-16 07:45:47 +0000

    18870

--- Baekjoon/18870/main.cc
+++ Baekjoon/18870/main.cc
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-vector<int> vec;
-vector<int> cmp;
+vector<int> vec;    // 정렬 후 중복제거
+vector<int> vec2;
 
 int main() {
     int n;
@@ -14,13 +14,13 @@ int main() {
         cin >> a;
 
         vec.push_back(a);
-        cmp.push_back(a);
+        vec2.push_back(a);
     }
 
     sort(vec.begin(), vec.end());
     vec.erase(unique(vec.begin(), vec.end()), vec.end());
     
     for(int i = 0; i < n; i++) {
-        cout << lower_bound(vec.begin(), vec.end(), cmp[i]) - vec.begin() << " ";
+        cout << lower_bound(vec.begin(), vec.end(), vec2[i]) - vec.begin() << " ";
     }
 }
