5556c0f182aba4d9324c6d3b9169a1aedd0dd6bd
BubblingSODA <go2gym365@gmail.com>
2023-07-18 12:00:51 +0000

    7569

--- Baekjoon/7569/main.cc
+++ Baekjoon/7569/main.cc
@@ -15,48 +15,53 @@ bool vis[101][101][101] = {false, };
 queue<pair<int, pair<int, int>>> q;
 
 void BFS() {
-        queue<pair<int, pair<int, int>>> future;
-        while(!q.empty()) {
-            int z = q.front().first;
-            int y = q.front().second.first;
-            int x = q.front().second.second;
+    while(1) {
+        
+    }
+    while(!q.empty()) {
+        queue<pair<int, pair<int, int>>> future;    
+        int z = q.front().first;
+        int y = q.front().second.first;
+        int x = q.front().second.second;
             
-            q.pop();
+        q.pop();
 
-            for(int i = 0; i < 6; i++) {
-                int nz = z + dz[i];
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        for(int i = 0; i < 6; i++) {
+            int nz = z + dz[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
-                if(vis[nz][ny][nx]) continue;
-                if(tomato[nz][ny][nx] == -1) continue;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
+            if(vis[nz][ny][nx]) continue;
+            if(tomato[nz][ny][nx] == -1) continue;
 
-                future.push({nz, {ny, nx}});
-                vis[nz][ny][nx] = true;
-            }
+            future.push({nz, {ny, nx}});
+            vis[nz][ny][nx] = true;
+        }
 
-            if(future.empty()) {
-                for(int k = 0; k < h; k++) {
-                    for(int i = 0; i < m; i++) {
-                        for(int j = 0; j < n; j++) {
-                            if(tomato[k][i][j] == 0 && !vis[k][i][j]) {
-                                cout << -1;
-                                return;
-                            }
+        if(future.empty()) {
+            for(int k = 0; k < h; k++) {
+                for(int i = 0; i < m; i++) {
+                    for(int j = 0; j < n; j++) {
+                        if(tomato[k][i][j] == 0 && !vis[k][i][j]) {
+                            cout << -1;
+                            return;
                         }
                     }
                 }
             }
-            q = future;
-            cnt++;
+            cout << cnt;
+            return;
         }
-    cout << cnt - 1;
-    return;
+        q = future;
+        cnt++;
+    }
 }
 
 int main() {
     cin >> n >> m >> h;
+
+    int t = 0;
 
     for(int k = 0; k < h; k++) {
         for(int i = 0; i < m; i++) {
