cf7cedf71f79d19e1fde6e266b5211d3a68c1ae3
BubblingSODA <go2gym365@gmail.com>
2023-07-22 10:50:22 +0000

    7662

---
+++ Baekjoon/7662/main.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        multiset<int> ms;
+
+        int n;
+        cin >> n;
+
+        for(int i = 0; i < n; i++) {
+            char c;
+            int num;
+
+            cin >> c >> num;
+        }
+    }
+}
