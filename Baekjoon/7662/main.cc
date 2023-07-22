f527cd8651857cd64d1efa088798fb164f8e4418
BubblingSODA <go2gym365@gmail.com>
2023-07-22 11:14:43 +0000

    7662

--- Baekjoon/7662/main.cc
+++ Baekjoon/7662/main.cc
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
     int t;
     cin >> t;
 
@@ -17,6 +19,26 @@ int main() {
             int num;
 
             cin >> c >> num;
+
+            if(c == 'I') {
+                ms.insert(num);
+            }
+            else if(c == 'D') {
+                if(ms.empty()) continue;
+                if(num == 1) {
+                    ms.erase(--ms.end());
+                }
+                else if(num == -1) {
+                    ms.erase(ms.begin());
+                }
+            }
+
+        }
+        if(ms.empty()) {
+            cout << "EMPTY" << "\n";
+        }
+        else {
+            cout << *(--ms.end()) << ' ' << *ms.begin() << "\n";
         }
     }
 }
