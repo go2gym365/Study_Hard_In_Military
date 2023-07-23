cf841bfbd5dd43b78351c0f5f7a1bcceb1afa46f
BubblingSODA <go2gym365@gmail.com>
2023-07-23 16:36:56 +0000

    5073

--- Baekjoon/5073/main.cc
+++ Baekjoon/5073/main.cc
@@ -4,20 +4,25 @@ using namespace std;
 
 int main() {
     while (1) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        
-        if(a == 0 && b == 0 && c == 0) break;
+        int tri[3];
 
-        int m = max(max(a, b), c);
-        if()
-        if(a == b && b == c) 
+        for (int i = 0; i < 3; i++) {
+            cin >> tri[i];
+        }
+
+        sort(tri, tri + 3);
+
+        if (tri[0] == 0 && tri[1] == 0 && tri[2] == 0)
+            break;
+
+        if (tri[2] >= tri[0] + tri[1])
+            cout << "Invalid\n";
+        else if (tri[0] == tri[1] && tri[1] == tri[2])
             cout << "Equilateral\n";
-        else if(a == b || b == c || c == b) 
+        else if (tri[0] == tri[1] || tri[1] == tri[2] || tri[2] == tri[1])
             cout << "Isosceles\n";
-        else if(a != b && b != c)
+        else if (tri[0] != tri[1] && tri[1] != tri[2]) 
             cout << "Scalene\n";
-        
         
     }
 }
