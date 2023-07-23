3b6f4a5d6cbb64818ea71d3ce9aedeff10f34957
BubblingSODA <go2gym365@gmail.com>
2023-07-23 14:39:07 +0000

    18430

--- Baekjoon/18430/main.cc
+++ Baekjoon/18430/main.cc
@@ -2,19 +2,31 @@
 
 using namespace std;
 
-vector<vector<int>> vec(5, vector<int>(5, 0));
-vector<vector<bool>> vis(5, vector<bool>(5, false));
+vector<vector<int>> vec(10, vector<int>(10, 0));
+vector<vector<bool>> vis(10, vector<bool>(10, false));
 
 int n, m;
 int ans = -987654321;
 
 int downRight(int y, int x) {
-    return vec[y][x] + vec[y + 1][x] + vec[y + 1][x + 1];
+    return vec[y][x] + vec[y + 1][x] * 2 + vec[y + 1][x + 1];
+}
+
+int rightDown(int y, int x) {
+    return vec[y][x] + vec[y][x + 1] * 2 + vec[y + 1][x + 1];
+}
+
+int upRight(int y, int x) {
+    return vec[y][x] + vec[y - 1][x] * 2 + vec[y - 1][x + 1];
+}
+
+int rightUp(int y, int x) {
+    return vec[y][x] + vec[y][x + 1] * 2 + vec[y - 1][x + 1];
 }
 
 void DFS(int y, int x, int sum) {
     if(x == m) {
-        x == 0;
+        x = 0;
         y++;
     }
     if(y == n) {
@@ -37,7 +49,31 @@ void DFS(int y, int x, int sum) {
             vis[y][x] = true;
             vis[y][x + 1] = true;
             vis[y + 1][x + 1] = true;
-            int temp = sum + 
+            int temp = sum + rightDown(y, x);
+            DFS(y, x + 1, temp);
+            vis[y][x] = false;
+            vis[y][x + 1] = false;
+            vis[y + 1][x + 1] = false;
+        }
+        if(x + 1 < m && y - 1 >= 0 && !vis[y - 1][x] && !vis[y - 1][x + 1]) {
+            vis[y][x] = true;
+            vis[y - 1][x] = true;
+            vis[y - 1][x + 1] = true;
+            int temp = sum + upRight(y, x);
+            DFS(y, x + 1, temp);
+            vis[y][x] = false;
+            vis[y - 1][x] = false;
+            vis[y - 1][x + 1] = false;
+        }
+        if(x + 1 < m && y - 1 >= 0 && !vis[y + 1][x] && !vis[y - 1][x + 1]) {
+            vis[y][x] = true;
+            vis[y][x + 1] = true;
+            vis[y - 1][x + 1] = true;
+            int temp = sum + rightUp(y, x);
+            DFS(y, x + 1, temp);
+            vis[y][x] = false;
+            vis[y][x + 1] = false;
+            vis[y - 1][x + 1] = false;
         }
     }
     DFS(y, x + 1, sum);
@@ -54,5 +90,5 @@ int main() {
 
     DFS(0, 0, 0);
 
-    
+    cout << ans;
 }
