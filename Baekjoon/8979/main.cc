5a2e3bbd097d630d05c745916d8ac347cec9a19d
BubblingSODA <go2gym365@gmail.com>
2023-07-24 06:45:35 +0000

    8979

--- Baekjoon/8979/main.cc
+++ Baekjoon/8979/main.cc
@@ -2,8 +2,40 @@
 
 using namespace std;
 
-vector<pair<int, pair<int, int>>> vec;
+vector<pair<pair<int, int>, pair<int, int>>> vec;
 
-int main() {
-    
+int n, want;
+
+bool cmp(pair<pair<int, int>, pair<int, int>> a, pair<pair<int, int>, pair<int, int>> b) {
+    if(a.first.second > b.first.second) 
+        return a.first.second > b.first.second;
+    else if(a.first.second == b.first.second) {
+        if(a.second.first == b.second.first) {
+            if(a.second.second == b.second.second) {
+                return a.first.first == want;
+            }
+            return a.second.second > b.second.second;
+        }
+        return a.second.first > b.second.first;
+    }
+    return false;
+}
+
+int main() {   
+    cin >> n >> want;
+    for(int i = 0; i < n; i++) {
+        int num, gold, silver, copper;
+
+        cin >> num >> gold >> silver >> copper;
+
+        vec.push_back({{num, gold}, {silver, copper}});
+    }
+
+    sort(vec.begin(), vec.end(), cmp);
+
+    for(int i = 0; i < n; i++) {
+        if(vec[i].first.first == want) {
+            cout << i + 1;
+        }
+    }
 }
