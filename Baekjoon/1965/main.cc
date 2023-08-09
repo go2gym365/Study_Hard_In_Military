fcd12198dfc76cddc8778a1425be2aa9c4a9ba2d
BubblingSODA <go2gym365@gmail.com>
2023-08-09 05:53:09 +0000

    1965

--- Baekjoon/1965/main.cc
+++ Baekjoon/1965/main.cc
@@ -2,22 +2,35 @@
 //LIS Algorithm
 using namespace std;
 
-int dp[1001];
+int dp[1001][1001];
 vector<int> vec;
 
 int n;
-int ans = 0;
 
-int solve(int cur) {
-    int &ret = dp[cur];
-    if(cur == n) return 0;
-    if(ret != 0) return ret;
-
-    ret = 1;
-
-    for(int i = 0; i  <n ; i++) {
-        solved(i)
+int solve(int prev, int cur) {
+    if (cur == n) return 0;
+    int ret = 0;
+    for(int i=cur; i<n; i++) {
+        if (prev < vec[i]) {
+            cout << vec[i] << " ";
+            ret = max(ret, solve(vec[i], i+1) + 1);
+        }
     }
+    cout << endl;
+    return ret;
+}
+
+int Solved(int prev, int cur) {
+    int &ret = dp[prev][cur];
+    if (cur == n) return 0;
+    if(ret != 0) return ret;
+    ret = 0;
+    for(int i = cur; i < n; i++) {
+        if(prev < vec[i]) {
+            ret = max(ret, Solved(vec[i], i + 1) + 1);
+        }
+    }
+    return ret;
 }
 
 int main() {
@@ -32,10 +45,7 @@ int main() {
         vec.push_back(a);
     }
 
-    solve(0);
+    cout << Solved(0, 0);
 
-    for(int i = 0; i < n; i++) {
-        ans = max(ans, dp[i]);
-    }
-    cout << ans;
+    return 0;
 }
---
+++ Baekjoon/1965/tempCodeRunnerFile.cc
@@ -0,0 +1,12 @@
+int solve(int prev, int cur) {
+//     if (cur == n) return 0;
+//     int ret = 0;
+//     for(int i=cur; i<n; i++) {
+//         if (prev < vec[i]) {
+//             cout << vec[i] << " ";
+//             ret = max(ret, solve(vec[i], i+1) + 1);
+//         }
+//     }
+//     cout << endl;
+//     return ret;
+// }
