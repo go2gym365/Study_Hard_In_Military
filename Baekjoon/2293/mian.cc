efcd7c45d73d2366e9fbc3ee04c7914dabfcbca5
BubblingSODA <go2gym365@gmail.com>
2023-08-09 08:36:05 +0000

    2293

--- Baekjoon/2293/mian.cc
+++ Baekjoon/2293/mian.cc
@@ -2,23 +2,38 @@
 
 using namespace std;
 
-int n;
+int n, k;
 
 vector<int> vec;
+int dp[10001][101];
 
-int solve(int sum) {
-    int ret = 0;
-    if(sum == 10) return 1;
-    if(sum > 10) return 0;
+int solve(int sum, int idx) {
+    int &ret = dp[sum][idx];
+    if(sum == k) return 1;
+    if(sum > k) return 0;
+    if(ret != 0) return ret;
 
-    for(int i = 0; i < n; i++) {
-        ret += solve(sum + vec[i]);
+    ret = 0;
+
+    for(int i = idx; i < n; i++) {
+        ret += solve(sum + vec[i], i);
     }
     return ret;
 }
 
+// int solve(int sum, int idx) {
+//     int ret = 0;
+//     if(sum == k) return 1;
+//     if(sum > k) return 0;
+
+//     for(int i = idx; i < n; i++) {
+//         ret += solve(sum + vec[i], i);
+//     }
+//     return ret;
+// }
+
 int main() {
-    cin >> n;
+    cin >> n >> k;
 
     for(int i = 0; i < n; i++) {
         int a;
@@ -27,5 +42,5 @@ int main() {
         vec.push_back(a);
     }
 
-    cout << solve(0);
+    cout << solve(0, 0);
 }
