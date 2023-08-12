af0b48b6bf08c93ac6f42cf2f97f26dbf1e0eb07
BubblingSODA <go2gym365@gmail.com>
2023-08-12 07:55:42 +0000

    15486

---
+++ Baekjoon/15486/main.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+vector<int> vec;
+int dp[1500001];
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        
+    }
+}
