6a47d8d88a20f81d3181e248dd5a2550368e1e74
BubblingSODA <go2gym365@gmail.com>
2023-08-14 02:20:28 +0000

    14002

--- Baekjoon/14002/main2.cc
+++ Baekjoon/14002/main2.cc
@@ -1,5 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main
+int n;
+vector<int> vec;
+
+int dp[1001], len[1001];
+
+void tr(int idx){ // 4
+    if(idx == dp[idx]) {
+        cout << vec[idx] << " ";
+        return;
+    }
+    tr(dp[idx]);
+    cout << vec[idx] << " ";
+}
+
+int main() {
+    int Max = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        vec.push_back(a);
+        dp[i] = i;
+        len[i] = 1;
+
+        for(int j = 0; j < i; j++) {
+            if(vec[j] < vec[i]) {
+                if(len[i] < len[j] + 1){
+                    len[i] = len[j] + 1;
+                    dp[i] = j;
+                }
+            }
+        }
+        if(len[i] > len[Max])
+            Max = i;
+    }
+    cout << len[Max] << "\n";
+    tr(Max);
+}
