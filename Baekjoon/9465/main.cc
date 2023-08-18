776c7146f59457d2705df2d4603dc936568821ca
BubblingSODA <go2gym365@gmail.com>
2023-08-18 23:15:48 +0000

    9465

--- Baekjoon/9465/main.cc
+++ Baekjoon/9465/main.cc
@@ -1,26 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int t;
+int t, n;
 
-vector<int> vec;
+vector<vector<int>> vec(2, vector<int>(100001, 0));
+int dp[2][100001];
+
+int solve(int y, int x) {
+    if (x == n)
+        return 0;
+    int &ret = dp[y][x];
+    if (ret != -1)
+        return ret;
+
+    ret = 0;
+    
+    if (y == 0) {
+        ret += max(ret, solve(y + 1, x + 1) + vec[y][x]);
+        ret += max(ret, solve(y + 1, x + 1) + vec[y][x]);
+        ret += max(ret, solve(y + 1, x + 1) + vec[y][x]);
+    }
+}
 
 int main() {
+    memset(dp, -1, sizeof(dp));
+
     cin >> t;
 
-    while(t--) {
-        int n;
+    while (t--) {
+
         cin >> n;
 
-        for(int i = 0; i < n; i++) {
-            int a;
-            cin >> a;
-            vec.push_back(a);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> vec[i][j];
+            }
         }
 
-        
+        solve(0, 0);
     }
-
-
 }
