78b8634f7149490463d06b9de4766fe2d5ad0fab
BubblingSODA <go2gym365@gmail.com>
2023-08-19 17:52:02 +0000

    Change form

--- Algotithm_Implement/Bubble_sort/Bubblesort.java
+++ Algotithm_Implement/Bubble_sort/Bubblesort.java
@@ -2,24 +2,26 @@ import java.util.*;
 import java.io.*;
 
 class Bubblesort {
-    static int [] arr = {2, 5, 3, 1, 4};
+    int [] arr = {2, 5, 3, 1, 4};
 
     public static void main(String[] args) {
-        bubbleSort();
+        Bubblesort bs = new Bubblesort();
 
-        for(int a : arr) {
+        bs.bubbleSort();
+
+        for(int a : bs.arr) {
             System.out.println(a);
         }
     }
 
-    public static void bubbleSort() {
+    public void bubbleSort() {
         for(int i = 0; i < arr.length; i++) {
             for(int j = 1; j < arr.length - i; j++) {
                 if(arr[j - 1] > arr[j]) {
                     int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
-                }  
+                }
             }
         }
     }
