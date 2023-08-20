5ce5179188e47cf93c7b599254bc2d7c2d4a3656
BubblingSODA <go2gym365@gmail.com>
2023-08-20 10:14:14 +0000

    16236

--- Baekjoon/16236/main.cc
+++ Baekjoon/16236/main.cc
@@ -1,129 +1,97 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-typedef vector<int> vi;
-int N, hunt = 0, sec = 0, size = 2;
-vector<vector<int>> space(20, vector<int>(20, 0));
-vector<vector<int>> visited(20, vector<int>(20, 0));
-int minDes = 987654321;
+
+int n;
+int sharkSize = 2;
+int feedcnt = 0;
+bool huntCheck = false;
+bool eat = false;
+int ans = 0;
+
+int sharkX;
+int sharkY;
+
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
-int minx = 0, miny = 0;
 
-void BFS()
-{
-    queue<pair<int, int>> q;
+vector<vector<int>> vec(21, vector<int>(21, 0));
+vector<vector<bool>> vis(21, vector<bool>(21, false));
 
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < N; j++)
-        {
-            if(space[i][j] == 9)
-            {
-                q.push({i, j});
-                miny = i;
-                minx = j;
-                visited[i][j] == 1;
-            }    
+void BFS(int yy, int xx) {
+    queue<pair<pair<int, int>, int>> q;
+    int time = 0;
+    q.push({{yy, xx}, 0});
+
+    while(!q.empty()) {
+        int y = q.front().first.first;
+        int x = q.front().first.second;
+        int cnt = q.front().second;
+
+        vis[y][x] == true;
+
+        if(vec[y][x] > 0 && vec[y][x] < sharkSize && time == cnt) {
+            if((sharkY > y) || (sharkY == y && sharkX > x)) {
+                sharkX = x;
+                sharkY = y;
+                continue;
+            }
         }
-    }
-
-    while(!q.empty())
-    {
-        int x = q.front().second;
-        int y = q.front().first;
         q.pop();
 
-        for(int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
+        for(int i = 0; i < 4; i++) {
             int ny = y + dy[i];
+            int nx = x + dx[i];
 
-            if(nx >= N || nx < 0 || ny >= N || ny < 0)
-                continue;
-            if(visited[ny][nx] || space[ny][nx] > size)
-                continue;
-            if(space[ny][nx] < size && visited[ny][nx] == 0)
-            {
-                if(space[ny][nx] > 0)
-                    space[ny][nx] *= -1;
-                if(minDes > visited[miny][minx] + 1)
-                    minDes = visited[miny][minx] + 1;
-                hunt++;
-                sec++;
-                q.push({ny, nx});
-                visited[miny][minx] += 1;
-            }
-            else if(space[ny][nx] == 0 || space[ny][nx] >= size)
-            {
-                q.push({ny, nx});
-                visited[miny][minx] += 1;
-            }
-        }
-
-        if(hunt == size)
-            size++;
-    }
-}
-
-bool compare(vi& a, vi& b) {
-    // a[0] -> distance
-    // a[1] -> y
-    // a[2] -> x
-    if(a[0] != b[0]) return a[0] < b[0];
-    if(a[1] != b[1]) return a[1] < b[1];
-    return a[2] < b[2];
-}
-
-int main()
-{
-    cin >> N;
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < N; j++)
-        {
-            cin >> space[i][j];
-        }
-    }
-
-    // 현재 물고기 위치 탐색
-    // curX, curY
-
-    BFS();
-
-    while(1) 
-    {
-        // 기저조건 : 모든 space를 전부 탐색 했을 떄 먹을게 없을 때
-        bool check = true;
-        for(int i = 0; i < N; i++)
-        {
-            for(int j = 0; j < N; j++)
-            {
-                
-                if(space[i][j] > 0)
-                {
-                    check = true;
-                    break;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if(vis[ny][nx]) continue;
+            if(vec[ny][nx] <= sharkSize) {
+                if(vec[ny][nx] > 0 && vec[ny][nx] < sharkSize && !eat) {
+                    eat = true;
+                    sharkX = nx;
+                    sharkY = ny;
+                    time = cnt + 1;
+                    ans += time;
                 }
-                else if(space[i][j] == 0)
-                    check = false;
+                else {
+                    q.push({{ny, nx}, cnt + 1});
+                    vis[ny][nx] = true;
+                }
             }
         }
-        if(check == false)
-        // 큐에다가 curX, curY bfs
-        // 먹을 수 있는 물고기 리스트 : eatable
-        
-        // (dist, y, x)
+    }
+}
 
-        sort(eatable.begin(), eatable.end(), compare);
-        // eatable[0]
+int main() {
+    cin >> n;
 
-        // 먹은 물고기를 0으로
-        // 현재 위치를 먹은 물고기 위치로
-
-        // 1초 증가
-        // 개수 올리고
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> vec[i][j];
+            if(vec[i][j] == 9) {
+                sharkX = j;
+                sharkY = i;
+                vec[i][j] = 0;
+            }
+        }
     }
 
-    cout << sec;
+    while(!huntCheck) {
+        fill(vis.begin(), vis.end(), vector<bool> (21, false));
+        BFS(sharkY, sharkX);
+        if(eat) {
+            eat = false;
+            feedcnt++;
+            vec[sharkY][sharkX] = 0;
+            if(feedcnt == sharkSize) {
+                sharkSize++;
+                feedcnt = 0;
+            }
+        }
+        else {
+            huntCheck = true;
+        }
+    }
+
+    cout << ans;
 }
