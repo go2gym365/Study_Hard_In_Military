ef4545e980d3e06603e8e401c8a3cb0deff8868f
BubblingSODA <go2gym365@gmail.com>
2023-08-21 01:29:16 +0000

    2003

---
+++ Baekjoon/2003/main.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> vec;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        vec.push_back(a);
+    }
+
+    int cnt = 0;
+    int end = 0;
+    int start = 0;
+
+    int sum = vec[0];
+
+    while(start <= end && end < n) {
+        if(sum < m) {
+            end++;
+            sum += vec[end];
+        }
+        else if(sum > m) {
+            sum -= vec[start];
+            start++;
+            if(start > end && start < n) {
+                end = start;
+                sum = vec[start];
+            }
+        }
+        else if(sum == m) {
+            cnt++;
+            end++;
+            sum += vec[end];
+        }
+    }
+    cout << cnt;
+}
