826b48d306e6b2e2bf1b762e44ccaffb1be6f190
BubblingSODA <go2gym365@gmail.com>
2023-08-24 07:52:36 +0000

    14442

---
+++ Baekjoon/14442/main.cc
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m, k;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+vector<string> vec(1001, "");
+int vis[1001][1001][11];
+
+int BFS() {
+    priority_queue<pair<pair<int, int>, int>> q;
+
+    vis[0][0][k] = 1;
+
+    q.push({{0, 0}, k});
+
+    while(!q.empty()) {
+        int y = q.top().first.first;
+        int x = q.top().first.second;
+        int cnt = q.top().second;
+
+        q.pop();
+
+        if(y == n - 1 && x == m - 1) {
+            return vis[y][x][cnt];
+        }
+
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+
+            if(vec[ny][nx] == '1' && cnt - 1 > 0) {
+                vis[ny][nx][cnt - 1] = vis[y][x][cnt] + 1;
+                q.push({{ny, nx}, cnt - 1});
+            }
+
+            if(vec[ny][nx] == '0' && vis[ny][nx][cnt] == 0) {
+                vis[ny][nx][cnt] = vis[y][x][cnt] + 1;
+                q.push({{ny, nx}, cnt});
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    cin >> n >> m >> k;
+
+    for(int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+    cout << BFS();
+}
