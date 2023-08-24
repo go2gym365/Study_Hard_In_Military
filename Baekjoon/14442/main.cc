96de4ea72d5810c5bd6eeb8899dadc87f7650004
BubblingSODA <go2gym365@gmail.com>
2023-08-24 08:03:30 +0000

    14442

--- Baekjoon/14442/main.cc
+++ Baekjoon/14442/main.cc
@@ -34,7 +34,7 @@ int BFS() {
 
             if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
 
-            if(vec[ny][nx] == '1' && cnt - 1 > 0) {
+            if(vec[ny][nx] == '1' && cnt > 0) {
                 vis[ny][nx][cnt - 1] = vis[y][x][cnt] + 1;
                 q.push({{ny, nx}, cnt - 1});
             }
