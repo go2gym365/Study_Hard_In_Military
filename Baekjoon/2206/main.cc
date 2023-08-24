f230a7bf08d078293d5693f1d63d3b8a3e2b315f
BubblingSODA <go2gym365@gmail.com>
2023-08-24 03:04:36 +0000

    2206

--- Baekjoon/2206/main.cc
+++ Baekjoon/2206/main.cc
@@ -11,30 +11,28 @@ int vis[1001][1001][2];
 
 int BFS(int yy, int xx) {
     queue<pair<pair<int, int>, int>> q;
-
     vis[yy][xx][1] = 1;
-
     q.push({{yy, xx}, 1});
 
     while(!q.empty()) {
         int y = q.front().first.first;
         int x = q.front().first.second;
         int crush = q.front().second;
+        q.pop();
 
         if(y == n - 1 && x == m - 1) {
             return vis[y][x][crush];
         }
 
-        q.pop();
 
         for(int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
             if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if(vec[ny][nx] == 1 && crush) {
-                vis[ny][nx][0] = vis[y][x][crush] + 1;
-                q.push({{ny, nx}, 0});
+            if(vec[ny][nx] == 1 && crush == 1) {
+                vis[ny][nx][crush - 1] = vis[y][x][crush] + 1;
+                q.push({{ny, nx}, crush - 1});
             }
             if(vec[ny][nx] == 0 && vis[ny][nx][crush] == 0) {
                 vis[ny][nx][crush] = vis[y][x][crush] + 1;
