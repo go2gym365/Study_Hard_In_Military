480b696e64616aeb401fa7882950f2393bc524db
Boiling SODA <go2gym365@gmail.com>
2023-09-12 18:26:41 +0000

    1202

--- Baekjoon/1202/multiset_lowerbound.cc
+++ Baekjoon/1202/multiset_lowerbound.cc
@@ -28,16 +28,14 @@ int main() {
     }
     
     
-    while(!bag.empty()) {
-        if(pq.empty()) break;
-        multiset<int>::iterator iter = bag.lower_bound(pq.top().second);
-        if(iter == bag.end()) {
-            pq.pop();
-            continue;
-        }
-        else bag.erase(iter);
-        answer += pq.top().first;
-        pq.pop();
+    while(!bag.empty()){
+        if(pq.empty()) break; //보석보다 가방이 많다면 (보석 다 담으면) 종료
+        pair<int,int> tmp = pq.top(); pq.pop();
+        //lower_bound를 활용하여 현재 보석의 무게와 같거나 더 큰 가방을 찾는다.
+        multiset<int>::iterator iter = bag.lower_bound(tmp.second);
+        if(iter==bag.end()) continue; //모든가방이 보석보다 작다면 못담음
+        else bag.erase(iter); //보석 담은 가방은 제거
+        answer+=tmp.first;
     }
 
     cout << answer;
