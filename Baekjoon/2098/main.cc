d24c9c549cd5fb6a54c41f3712c2fcf353a12d53
BubblingSODA <go2gym365@gmail.com>
2023-10-03 06:07:40 +0000

    2098

---
+++ Baekjoon/2098/main.cc
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+
+int n;
+int dp[16][1<<16]; //2^16
+vector<vector<int>> input(16, vector<int>(16, 0));
+
+int dfs(int cur, int vis) {
+    //기저 조건
+    if(vis == (1 << n) - 1) {
+        if(input[cur][0] == 0) {
+            return INF;
+        }
+        return input[cur][0];
+    }
+
+    int &ret = dp[cur][vis];
+
+    if(vis == (1<<n)-1) {
+        return ret;
+    }
+
+    ret = INF;
+
+    for(int i = 0; i < n; i++) {
+        if(input[cur][i] == 0) //길이 없으면
+            continue;
+        if((vis & (1<<i)) == (1<<i))
+            continue;
+        ret = min(ret, dfs(i, vis | 1<<i) + input[cur][i]);
+    }
+
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> input[i][j];
+        }
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << dfs(0, 1);
+    
+}
