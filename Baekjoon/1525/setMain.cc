a63a09970f902b1aafac10164bc4aa2306d13984
BubblingSODA <go2gym365@gmail.com>
2023-12-12 08:01:35 +0000

    solve 1525 using set

---
+++ Baekjoon/1525/setMain.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int graph[3][3];
+
+unordered_set<string> s;
+
+string str = "";
+const string answer = "123456780";
+
+
+int bfs() {
+    queue<pair<string, int>> q;
+    q.push({str, 0});
+    
+    while(!q.empty()) {
+        string curState = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
+        
+        if(curState == answer) {
+            return cnt;
+        }
+        
+        int zero = curState.find('0');
+        int x = zero / 3;
+        int y = zero % 3;
+        
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= 3 || ny >= 3) continue;
+            
+            string temp = curState;
+            
+            swap(temp[3*x + y], temp[3*nx+ny]);
+            
+            if(s.find(temp) == s.end()) {
+                s.insert(temp);
+                q.push({temp, cnt+1});
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> graph[i][j];
+            str += (graph[i][j] + '0');
+        }
+    }
+    
+    cout << bfs();
+}
