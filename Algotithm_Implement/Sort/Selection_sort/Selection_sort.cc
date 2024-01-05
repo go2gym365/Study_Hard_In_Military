c1b4979be50099ab12d2ac556fc65218b0010b65
BubblingSODA <go2gym365@gmail.com>
2024-01-05 05:22:27 +0000

    Implement selection sort

--- Algotithm_Implement/Sort/Selection_sort/Selection_sort.cc
+++ Algotithm_Implement/Sort/Selection_sort/Selection_sort.cc
@@ -4,35 +4,43 @@ using namespace std;
 
 vector<int> arr;
 
-void selectionSort() {
-    //마지막 값은 자동 정렬이 되었기 때문에 n-1
-    for(int i = 0; i < arr.size() - 1; i++) {
-        //최솟값을 가진 인덱스를 저장
-        int minIdx = i;
-        //최솟값을 저장하여 바꾸기때문에 매번 i까지는 정렬이 됨
-        for(int j = i + 1; j < arr.size(); j++) {
-            if(arr[minIdx] > arr[j]) {
-                minIdx = j;
-            }
-        }
-        //최솟값을 가진곳을 확인 했기 때문에 바꿔줌
-        swap(arr[minIdx], arr[i]);
-    }
-}
-
-int main() {
-    int n;
-    cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        arr.push_back(a);
-    }
-
-    selectionSort();
-
+void Print() {
     for(int a : arr) {
         cout << a << " ";
     }
+    cout << "\n";
+}
+
+void selectionSort() {
+    clock_t start, finish;
+    double duration;
+    
+    start = clock();
+
+    for(int i = 0; i < arr.size(); i++) {
+        int minValueIdx = i;
+        for(int j = i+1; j < arr.size(); j++) {
+            if(arr[minValueIdx] > arr[j]) {
+                minValueIdx = j;
+            }
+        }
+        if(minValueIdx != i) {
+            swap(arr[i], arr[minValueIdx]);
+        }
+    }
+
+    finish = clock();
+    
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    Print();
+    cout << duration << "초" << "\n";
+}
+
+int main() {
+    
+    for(int i = 0; i < 2500; i++) {
+        arr.push_back(rand() % 100);
+    }
+
+    selectionSort();
 }
