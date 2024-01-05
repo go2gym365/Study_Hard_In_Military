63ecd8f21e6678ea8c327ada0f8fa2d2593db182
BubblingSODA <go2gym365@gmail.com>
2024-01-05 05:19:22 +0000

    Implement Bubble sort

--- Algotithm_Implement/Sort/Bubble_sort/Bubble_sort.cc
+++ Algotithm_Implement/Sort/Bubble_sort/Bubble_sort.cc
@@ -1,32 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<int> arr;
 
+void Print() {
+    for (int a : arr) {
+        cout << a << " ";
+    }
+    cout << "\n";
+}
+
 void bubbleSort() {
-    for(int i = 0; i < arr.size(); i++) {
-        for(int j = 0; j < arr.size() - 1; j++) {
-            if(arr[j] > arr[j+1]) {
+    clock_t start, finish;
+    double duration;
+
+    start = clock();
+
+    for (int i = 0; i < arr.size() - 1; i++) {
+        bool isSwap = false;
+        // 뒤에서 i개는 정렬이 무조건 되기 때문에 i씩 빼주기
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
         }
+        if (!isSwap)
+            break;
     }
+
+    finish = clock();
+
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    Print();
+    cout << duration << "초" << "\n";
 }
 
 int main() {
-    int n;
-    cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        arr.push_back(a);
+    // int n;
+    // cin >> n;
+
+    for (int i = 0; i < 2500; i++) {
+        // int a;
+        // cin >> a;
+        arr.push_back(rand() % 100);
     }
 
     bubbleSort();
-
-    for(int a : arr) {
-        cout << a << " ";
-    }
 }
---
+++ Algotithm_Implement/Sort/Bubble_sort/tempCodeRunnerFile.cc
@@ -0,0 +1,1 @@
+        if(!isSwap) break;
