5b3f1992089310a243ae4d1201a89bb22d4a97ec
BubblingSODA <go2gym365@gmail.com>
2024-01-05 08:09:57 +0000

    Implementation Insertion sort

--- Algotithm_Implement/Sort/Insertion_sort/Insertion_sort.cc
+++ Algotithm_Implement/Sort/Insertion_sort/Insertion_sort.cc
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int n;
+
 vector<int> arr;
 
 void Print() {
@@ -32,12 +34,14 @@ void Insertion_sort() {
 
     duration = (double)(finish - start) / CLOCKS_PER_SEC;
 
-    Print();
-    cout << duration << "초" << "\n";
+    //Print();
+    cout << "Insertion sort로 " << n << "개 원소 정렬에 걸린 시간 " << duration << "초" << "\n";
 }
 
 int main() {
-    for (int i = 0; i < 2500; i++) {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
         arr.push_back(rand() % 100);
     }
 
